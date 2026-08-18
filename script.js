4e3b09a46d384f11a4b0f6bb77f6618ba92d9452
adolph-CEO <adolph@yichih.com.tw>
2026-08-18 10:25:26 +0800

    Deploy new design package: script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿document.querySelectorAll('a[data-cta]').forEach((link) => {
+  link.addEventListener('click', () => {
+    const position = link.dataset.cta;
+    if (window.gtag) window.gtag('event', 'line_click', { cta_position: position });
+    if (window.fbq) window.fbq('trackCustom', 'LineClick', { position });
+  });
+});
